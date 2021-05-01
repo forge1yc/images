71df935fcf33978878174da7db2463b96ed6764b
houyichao_i <houyichao_i@didiglobal.com>
2021-05-01 13:34:36 +0800

    [new] 添加web logo

---
+++ acp.sh
@@ -0,0 +1,14 @@
+##########################################################################
+# File Name: acp.sh
+# Author: houyichao
+# mail: houyichaochao@gmail.com
+# Created Time: 六  5/ 1 12:26:33 2021
+ # Last modified: 2021-05-01 12:26:33
+#########################################################################
+#!/bin/bash
+
+
+git add .
+git commit -m  "[new] $1"
+git push 
+
